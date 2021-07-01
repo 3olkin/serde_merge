09385d867982b95b272fde3e28385cc8af532f23
Z4RX <iamz4rx@gmail.com>
2021-07-01 21:12:42 +0300

    add simple example

---
+++ examples/map_with_map.rs
@@ -0,0 +1,15 @@
+use serde_merge;
+use std::collections::HashMap;
+
+fn main() {
+    let mut map1: HashMap<&str, &str> = HashMap::new();
+    map1.insert("key1", "value1");
+    map1.insert("key2", "value2");
+    map1.insert("key3", "value3");
+    let mut map2: HashMap<&str, &str> = HashMap::new();
+    map2.insert("key4", "value4");
+    map2.insert("key5", "value5");
+    map2.insert("key6", "value6");
+    let result: serde_merge::Map = serde_merge::mmerge(map1, map2).unwrap();
+    println!("{:#?}", result);
+}
